d663828f649da4dcc5c009626a1b02c58607dac2
Prasheel Soni <prasheelsoni11@gmail.com>
2020-06-22 17:22:46 +0530

    add build on root

---
+++ precache-manifest.ac506fd4f440dcd93d6852f2946941cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "afc418eaa61762bbbd9d",
+    "url": "/static/js/5.a2cb3df1.chunk.js"
+  },
+  {
+    "revision": "72a6ddb5b010a4081584",
+    "url": "/static/css/main.95c803d8.chunk.css"
+  },
+  {
+    "revision": "a3122a8a7a434a88cc77",
+    "url": "/static/js/runtime~main.885f4862.js"
+  },
+  {
+    "revision": "ceb13ee0d9da80f67f96",
+    "url": "/static/js/2.3a942456.chunk.js"
+  },
+  {
+    "revision": "0ee2502ab8ae5c3c618e",
+    "url": "/static/js/3.460c5d6c.chunk.js"
+  },
+  {
+    "revision": "37d8179a24a348acfe6a",
+    "url": "/static/js/4.eb1cd49e.chunk.js"
+  },
+  {
+    "revision": "72a6ddb5b010a4081584",
+    "url": "/static/js/main.9ad88cc6.chunk.js"
+  },
+  {
+    "revision": "50cbebbe891909862a0b",
+    "url": "/static/js/6.053b16a1.chunk.js"
+  },
+  {
+    "revision": "3a8fe397210173afd09a",
+    "url": "/static/js/7.ecc5e5f4.chunk.js"
+  },
+  {
+    "revision": "18e898eaf491dc6ac8cb",
+    "url": "/static/js/8.9c203098.chunk.js"
+  },
+  {
+    "revision": "417cd1fbb53ec2c020a8",
+    "url": "/static/js/9.6061c6fd.chunk.js"
+  },
+  {
+    "revision": "26c9d5b4d380cddf5a3e64ec2aa4f921",
+    "url": "/index.html"
+  }
+];
